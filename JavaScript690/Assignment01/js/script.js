a6ce280c6e1d834579d58b82bed74dcad792120d
HerculusHub <140985834+HerculusHub@users.noreply.github.com>
2026-02-18 10:52:17 -0800

    changes

--- JavaScript690/Assignment01/js/script.js
+++ JavaScript690/Assignment01/js/script.js
@@ -19,17 +19,18 @@ let myLibraryFunction;
 
 let firstNum=100;  //Numeric literal
 let firstMessage="Hello, World!";   //String literal
-let answerBoolean= ture; //Boolean literal
+let answerBoolean=true; //Boolean literal
 let emptyValue=null    //Null literal expression
 
 // 3
 // Two examples of complex / variable expressions
-let a=50;
-let b=5;
-let distance=a/b+5*(a-b);
+let a1=50;
+let b1=5;
+let distance=a1/b1+5*(a1-b1);
 console.log(distance);
 
 let nameUser = "Mike";
 let userAge = 37;
 let answerMessage = nameUser + " is " + (userAge >= 18 ? "an adult" : "a minor") + ".";
 console.log(answerMessage);
+
