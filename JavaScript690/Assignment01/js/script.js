3c68c8afeebbbb7b1bf7c3bbd3cb2752d9097b47
Bert <140985834+HerculusHub@users.noreply.github.com>
2026-02-17 20:23:50 -0800

    changes

--- JavaScript690/Assignment01/js/script.js
+++ JavaScript690/Assignment01/js/script.js
@@ -1,1 +1,35 @@
 // STEP 1
+// STEP 2
+// STEP 3
+
+// 1 Convert the following highlighted identifiers to Camel Case notation:
+
+// let some_month
+let someMonth;
+// function the Month()
+function theMonth() {}
+// let current-month
+let currentMonth;
+// let summer_month
+let summerMonth;
+// let MyLibrary-function
+let myLibraryFunction;
+
+// 2 
+
+let num=100;  //Numeric literal
+let message="Hello, World!";   //String literal
+let answer= ture; //Boolean literal
+let emptyValue=null    //Null literal expression
+
+// 3
+// Two examples of complex / variable expressions
+let a=50;
+let b=5;
+let distance=a/b+5*(a-b);
+console.log(distance);
+
+let name = "Mike";
+let age = 37;
+let message = name + " is " + (age >= 18 ? "an adult" : "a minor") + ".";
+console.log(message);
